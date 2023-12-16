904b0c9b046c0d4453dea5bc87934b983e888135
goldoverlord <116792525+goldoverlord@users.noreply.github.com>
2023-12-16 19:24:53 +0200

    19:24

--- js/main.js
+++ js/main.js
@@ -1,24 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var headerNavToggle = document.querySelector('.js-header-nav-toggle');
-  headerNavToggle.addEventListener('click', function (event) {
+var headerNavToggle = document.querySelector('.js-header-nav-toggle');
+headerNavToggle.addEventListener('click', function (event) {
     document.body.classList.toggle('is-menu-show');
-  });
+});
 
+  // Код для вибору мови
   var selectElement = document.getElementById('CurrencySelect');
+  var uaContent = document.getElementById('uaContent');
+  var enContent = document.getElementById('enContent');
+
   selectElement.addEventListener('change', function () {
     var selectedValue = this.value;
-    var uaContent = document.getElementById('uaContent');
-    var enContent = document.getElementById('enContent');
-
-    if (selectedValue === 'ua') {
-      uaContent.style.display = 'block';
-      enContent.style.display = 'none';
-    } else {
-      uaContent.style.display = 'none';
-      enContent.style.display = 'block';
-    }
+    uaContent.style.display = selectedValue === 'ua' ? 'block' : 'none';
+    enContent.style.display = selectedValue === 'ua' ? 'none' : 'block';
   });
 
+  // Код для пошуку
   var searchInput = document.getElementById('searchInput');
   var searchBtn = document.getElementById('searchBtn');
   var searchIcon = document.querySelector('.form-search__icon');
@@ -40,5 +37,22 @@ document.addEventListener('DOMContentLoaded', function () {
         searchInput.classList.remove('show');
       }
     }
+  });
+  
+  // Код для паралаксу
+  var paralaxContainer = document.querySelector('.paralax');
+  var paralaxImages = document.querySelectorAll('.paralax__bg-img');
+
+  paralaxContainer.addEventListener('mousemove', function (e) {
+    var rect = paralaxContainer.getBoundingClientRect();
+    var offsetX = (e.clientX - rect.left) / rect.width - 0.5;
+    var offsetY = (e.clientY - rect.top) / rect.height - 0.5;
+
+    paralaxImages.forEach(function (img, index) {
+      var translateX = offsetX * (index + 1) * 20;
+      var translateY = offsetY * (index + 1) * 20;
+
+      img.style.transform = 'translate(' + translateX + 'px, ' + translateY + 'px)';
+    });
   });
 });
