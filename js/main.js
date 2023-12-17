2b24076678f456f9d3b5c560d8118ba102979ea0
goldoverlord <116792525+goldoverlord@users.noreply.github.com>
2023-12-17 19:35:23 +0200

    ---

--- js/main.js
+++ js/main.js
@@ -1,18 +1,45 @@
+$(document).ready(function(){
+  var slickSlider = $('.js-automated-section-slick-slider');
 
-  $(document).ready(function(){
-    // slider slick init
-    $('.js-automated-section-slick-slider').slick({
-      autoplay: false,
-      dots: true,
-      infinite: true,
-      speed: 400,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      prevArrow: $('.js-intro-slider-control .slick-prev'),
-      nextArrow: $('.js-intro-slider-control .slick-next'),
-      appendDots: '.js-intro-slider-dots',
-      customPaging: function(slider, i) {
-        return '<span class="dot"></span>';
+  slickSlider.slick({
+    autoplay: false,
+    dots: true,
+    infinite: false,
+    speed: 400,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $('.js-intro-slider-control .slick-prev'),
+    nextArrow: $('.js-intro-slider-control .slick-next'),
+    appendDots: '.js-intro-slider-dots',
+    customPaging: function(slider, i) {
+      return '<span class="dot"></span>';
+    },
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        }
       }
-    });
+    ]
   });
+
+  if (slickSlider.slick('slickCurrentSlide') === 0) {
+    updateDotsClass(0);
+  }
+
+  slickSlider.on('afterChange', function(event, slick, currentSlide){
+    updateDotsClass(currentSlide);
+  });
+
+  function updateDotsClass(currentSlide) {
+    $('.js-intro-slider-dots .dot').removeClass('slick-active');
+    $('.js-intro-slider-dots .dot').slice(currentSlide, currentSlide + 3).addClass('slick-active');
+  }
+});
