a1e2f63802a16559f6edc1f8f0680860b07f9525
goldoverlord <116792525+goldoverlord@users.noreply.github.com>
2023-12-17 18:31:23 +0200

    aaaa

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 
   $(document).ready(function(){
-    $('.slider').slick({
-      dots: false,
+    // slider slick init
+    $('.js-automated-section-slick-slider').slick({
+      autoplay: false,
+      dots: true,
+      infinite: true,
+      speed: 400,
+      slidesToShow: 3,
+      prevArrow: $('.js-intro-slider-control .slick-prev'),
+      nextArrow: $('.js-intro-slider-control .slick-next'),
       appendDots: '.js-intro-slider-dots',
       customPaging: function(slider, i) {
         return '<span class="dot"></span>';
